74aa433edad0c3fb0a9cda6429b7ceafc61576cd
amalcaraz <amalcaraz89@gmail.com>
2019-08-29 01:01:45 +0200

    Added react-native-unimodules (needed by some expo libs like local-auth and secure-store)

---
+++ scripts/clean.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo "rm -rf ios/build..."
+rm -rf ios/build
+
+echo "rm -rf ios/Pods..."
+rm -rf ios/Pods
+
+echo "rm -rf android/app/build..."
+rm -rf android/app/build
+
+echo "Removed all Xcode derived data..."
+rm -rf ~/Library/Developer/Xcode/DerivedData
+
+echo "rm -rf lib... (for Flow)"
+rm -rf lib
+
+echo "watchman watch-del-all..."
+watchman watch-del-all
+
+echo "rm -rf node_modules..."
+rm -rf node_modules
+
+echo "npm cache clean --force"
+npm cache clean --force
+
+echo "rm -rf $TMPDIR/react-*..."
+rm -rf $TMPDIR/react-*
+
+echo "rm -rf $TMPDIR/haste-map-react-native-packager-*..."
+rm -rf $TMPDIR/haste-map-react-native-packager-*
+
+echo "rm -rf $TMPDIR/metro-bundler-cache-*"
+rm -rf $TMPDIR/metro-bundler-cache-*
+
+echo "rm -rf $TMPDIR/react-native-packager-cache-*"
+rm -rf $TMPDIR/react-native-packager-cache-*
