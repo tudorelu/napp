e51f5386694bf6e7b0e52544a2818e9c84faa27f
Tudor <tudor.barbu7@gmail.com>
2019-09-10 17:01:58 +1000

    Updated android to accept react-native-screens

--- android/app/src/main/java/com/napp/MainApplication.java
+++ android/app/src/main/java/com/napp/MainApplication.java
@@ -3,6 +3,7 @@ package com.napp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.rnscreens.RNScreensPackage;
 import com.swmansion.reanimated.ReanimatedPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -33,6 +34,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNScreensPackage(),
             new ReanimatedPackage(),
             new VectorIconsPackage(),
             new RNGestureHandlerPackage(),
